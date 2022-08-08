d874ada56287f040ccc628cda0d4dad3468bf01e
esrabecker <esraa.becker@gmail.com>
2022-08-08 18:50:03 +0200

    Create Day-1 Lab -Copy2.py

---
+++ Day-1 Lab -Copy2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day -1 Lab Exercise 
+
+# ## Purpose of this notebook
+
+# I would like to introduce you my husband David. And we'll talk about his favorite holiday destination "Eibsee".
+
+# ## Information about the target
+
+# In[5]:
+
+
+name = input()
+
+
+# In[8]:
+
+
+from IPython.display import Image
+Image(filename='david.jpeg')
+
+
+# ## Favorite holiday destination
+
+# In[10]:
+
+
+from IPython.display import Image
+Image("https://www.merkur.de/bilder/2021/12/14/91178512/27695988-eibsee-vea.jpg")
+
+
+# ## Video of the holiday destination
+
+# [Here's the video of Eibsee](https://www.youtube.com/watch?v=bzRerlhaHNI)
+
+# ## Favorite things to do
+
+# - Hiking
+# - Camping
+# - Picnic
+
+# First I have used input() function to enter selected person's name and then I used import image function to add the image of the person and his favorite holiday destination.
